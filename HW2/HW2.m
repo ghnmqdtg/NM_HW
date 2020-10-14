e7c699795f943c7b0dbd4838bfb3ab744db55fe2
ghnmqdtg <ghnmqdtg@gmail.com>
2020-10-14 22:50:33 +0800

    Feat: add 20201008 class practices and HW3

--- HW2/HW2.m
+++ HW2/HW2.m
@@ -1,6 +1,7 @@
 clear all
 close all
 format long
+hold on;
 
 % the border of the start and the end
 border_left = 1; border_right = 29;
@@ -25,7 +26,7 @@ buildings = [
 
 % linspace(x1, x2, N) generates N numbers from x1 to x2
 % initialize height of skyline with zeros
-y = linspace(0, 0, border_right - border_left + 1)
+y = linspace(0, 0, border_right - border_left + 1);
 
 % scan from the first building to the last one
 for i = 1:length(buildings)
@@ -33,14 +34,28 @@ for i = 1:length(buildings)
 	for x = buildings(i, left):(buildings(i, right) - 1)
 		% choose the highest one as the final height
 		if buildings(i, height) > y(x)
-			y(x) = buildings(i, height)
+			y(x) = buildings(i, height);
 		end
 	end
 end
+stairs(border_left-1:1:border_right, [0 y]);
+y
+
+% scan from the first building to the last one
+for i = 1:length(buildings)
+	% minus 1 for avoiding count one more
+	for x = buildings(i, left):(buildings(i, right))
+		% choose the highest one as the final height
+		if buildings(i, height) > y(x)
+			y(x) = buildings(i, height);
+		end
+	end
+end
+y
 
 % set the size of the figure
 % [x_min x_max y_min y_max]
 axis([border_left-1 border_right+1 0 20])
-hold on;
+
 % plot the figure
 stairs(border_left-1:1:border_right, [0 y]);
