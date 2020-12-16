79bb0d8db56e26e3a3fe300676969da20c87ffe4
ghnmqdtg <ghnmqdtg@gmail.com>
2020-12-16 15:42:22 +0800

    Feat: Cascade splines in HW7 Q1 as one

--- HW7/HW7_Q1_a.m
+++ HW7/HW7_Q1_a.m
@@ -57,14 +57,20 @@ hold on;
 plot(X, Y, 'o');
 
 % Plot the estimated line
+x_tmp = [];
+y_tmp = [];
+
 for i = 1 : n - 1
     x = X(i) : 0.25 : X(i + 1);
     y = @(x, i) s(i, 1) + s(i, 2) * (x - X(i)) + s(i, 3) * (x - X(i)).^ 2 + s(i, 4) * (x - X(i)).^ 3;
-    plot(x, y(x, i));
+    x_tmp = [x_tmp x];
+    y_tmp = [y_tmp y(x, i)];
 end
+
+plot(x_tmp, y_tmp);
 
 % Config of figure
 title('Not-a-knot Spline');
 xlabel('x coordinate')
 ylabel('y coordinate')
-legend('Given Data', 'Splines', 'location', 'best');
+legend('Given Data', 'Spline', 'location', 'best');
