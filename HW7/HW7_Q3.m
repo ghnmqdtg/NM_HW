78679f1c86692f6c38f8c1475b0898b6fdd7f462
ghnmqdtg <ghnmqdtg@gmail.com>
2020-12-16 17:02:00 +0800

    Feat: Add HW7 Q3 and its result

---
+++ HW7/HW7_Q3.m
@@ -0,0 +1,25 @@
+clear;
+close all;
+format long;
+
+X = [1 2 3 5 7 8];
+Y = [3 6 19 99 291 444];
+desired_x = 4;
+
+yint = Lagrange(X, Y, desired_x);
+
+%-------------------- Plot the figure --------------------%
+hold on;
+
+% Plot data points
+plot(X, Y, 'o');
+
+x = 0 : 0.25 : X(length(X)) + 1;
+plot(x, Lagrange(X, Y, x));
+plot(desired_x, yint, '.', 'MarkerSize', 30, 'color', 'r');
+
+% Config of figure
+title('Lagrange Interpolating Polynomial');
+xlabel('x coordinate')
+ylabel('y coordinate')
+legend('Given Data', 'Polynomial', 'Desired Data', 'location', 'best');
