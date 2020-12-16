9bbac737f98413e8074f8286b235fc9128ce4f46
ghnmqdtg <ghnmqdtg@gmail.com>
2020-12-16 17:32:06 +0800

    Misc: Modify HW7 Q2 and Q3 to show polynomials with different order

--- HW7/HW7_Q3.m
+++ HW7/HW7_Q3.m
@@ -5,8 +5,11 @@ format long;
 X = [1 2 3 5 7 8];
 Y = [3 6 19 99 291 444];
 desired_x = 4;
+yint = [];
 
-yint = Lagrange(X, Y, desired_x);
+for i = 2 : 4
+    yint = [yint Lagrange(X(1:i), Y(1:i), desired_x)];
+end
 
 %-------------------- Plot the figure --------------------%
 hold on;
@@ -15,11 +18,17 @@ hold on;
 plot(X, Y, 'o');
 
 x = 0 : 0.25 : X(length(X)) + 1;
-plot(x, Lagrange(X, Y, x));
-plot(desired_x, yint, '.', 'MarkerSize', 30, 'color', 'r');
+
+% order = points - 1, points >= 2
+% polynomials for order over 3 have the same result
+for i = 2 : 4
+    plot(x, Lagrange(X(1:i), Y(1:i), x));
+end
+
+plot(desired_x, yint, '.', 'MarkerSize', 10, 'color', 'r');
 
 % Config of figure
 title('Lagrange Interpolating Polynomial');
 xlabel('x coordinate')
 ylabel('y coordinate')
-legend('Given Data', 'Polynomial', 'Desired Data', 'location', 'best');
+legend('Given Data', 'Order 1', 'Order 2', 'Order 3', 'Desired Data', 'location', 'best');
