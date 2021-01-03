42155177d52bb439823d7f9ec3ee8584b9de75fc
ghnmqdtg <ghnmqdtg@gmail.com>
2021-01-03 21:17:13 +0800

    Feat: Add HW8 Q1.b

---
+++ HW8/HW8_Q1_b.m
@@ -0,0 +1,30 @@
+clear
+close all
+format long
+
+% The given function
+func = @(x) 8 + 4.*cos(x);
+lower_bound = 0;
+upper_bound = pi / 2;
+actual = 8 * (upper_bound - lower_bound) + 4 * (sin(upper_bound) - sin(lower_bound));
+
+% Single application of the trapezoidal rule
+estimated = (upper_bound - lower_bound) * (func(upper_bound) + func(lower_bound)) / 2;
+% The error
+error_value = abs(estimated - actual);
+
+% Print out the information
+fprintf('Q1-b: Single application of the trapezoidal rule\n');
+fprintf('Estimated Value: %.15f\n', estimated);
+fprintf('Actual Value:    %.15f\n', actual);
+fprintf('Error:           %.15f\n', error_value);
+
+% Plot the figure
+hold on;
+x = -0.5: 0.001: 2;
+plot(x, func(x));
+plot([lower_bound lower_bound upper_bound upper_bound] , [0 func(lower_bound) func(upper_bound) 0]);
+xlim([lower_bound-0.5 upper_bound+0.5]);
+ylim([0 13])
+title('Single application of the trapezoidal rule')
+legend("Actual function", "Estimated");
