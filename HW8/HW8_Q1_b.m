d123f6e134314a9435757d93b02788c62c06bb64
ghnmqdtg <ghnmqdtg@gmail.com>
2021-01-03 23:20:05 +0800

    Misc: little edit HW8 Q1.b

--- HW8/HW8_Q1_b.m
+++ HW8/HW8_Q1_b.m
@@ -11,19 +11,21 @@ actual = 8 * (upper_bound - lower_bound) + 4 * (sin(upper_bound) - sin(lower_bou
 % Single application of the trapezoidal rule
 estimated = (upper_bound - lower_bound) * (func(upper_bound) + func(lower_bound)) / 2;
 % The error
-error_value = abs(estimated - actual);
+error_value = abs(estimated - actual) / actual;
 
 % Print out the information
 fprintf('Q1-b: Single application of the trapezoidal rule\n');
-fprintf('Estimated Value: %.15f\n', estimated);
-fprintf('Actual Value:    %.15f\n', actual);
-fprintf('Error:           %.15f\n', error_value);
+fprintf('Estimated Value: %.10f\n', estimated);
+fprintf('Actual Value:    %.10f\n', actual);
+fprintf('Percent Error:   %.2f%%\n', error_value * 100);
 
 % Plot the figure
 hold on;
 x = -0.5: 0.001: 2;
+x_step = [lower_bound lower_bound upper_bound upper_bound];
+y_step = [0 func(lower_bound) func(upper_bound) 0];
 plot(x, func(x));
-plot([lower_bound lower_bound upper_bound upper_bound] , [0 func(lower_bound) func(upper_bound) 0]);
+plot(x_step , y_step);
 xlim([lower_bound-0.5 upper_bound+0.5]);
 ylim([0 13])
 title('Single application of the trapezoidal rule')
