5e7cf4c67fb50aa9ee82b24fd07d2e716dfb3dc3
ghnmqdtg <ghnmqdtg@gmail.com>
2021-01-05 15:32:20 +0800

    Feat: Add HW8 Q3 and its results

---
+++ HW8/HW8_Q3_b.m
@@ -0,0 +1,14 @@
+clear
+close all
+format long
+
+% The given function
+func = @(x, y) (x.*exp(-(x.^2 + y.^2)));
+[X, Y] = meshgrid(-3:.01:3);
+
+% Plot the figure
+mesh(X, Y, func(X, Y));
+xlabel('x coordinate');
+ylabel('y coordinate');
+zlabel('Z');
+title('Generate the Function')
