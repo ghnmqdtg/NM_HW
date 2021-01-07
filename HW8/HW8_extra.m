ff1c25c5e97a4aadd3d1fdeb08e15bedc3dc577e
ghnmqdtg <ghnmqdtg@gmail.com>
2021-01-07 21:49:41 +0800

    Feat: Add HW8 Q5, extra and their results

---
+++ HW8/HW8_extra.m
@@ -0,0 +1,36 @@
+clear;
+close all;
+format long;
+
+% e^t
+% y = @(t) exp(t);
+% dydt = @(t, y) 1 * y;
+% tspan = [0 2];
+
+% sin(t)
+% y = @(t) sin(t);
+% dydt = @(t, y) cos(t);
+% tspan = [0 7];
+
+% t*sin(t)
+y = @(t) sin(t)./t;
+dydt = @(t, y) (t.*cos(t)-sin(t))./(t.^2);
+tspan = [-10 10];
+
+y0 = y(tspan(1));
+h = 0.1;
+
+% y is obtain by iteration
+[t_aprox, y_aprox] = explicit_EulerODE(dydt, tspan, y0, h);
+
+% Plot the figure
+figure(1)
+hold on;
+func_name = func2str(y);
+t_actual = tspan(1): 0.005: tspan(end);
+plot(t_actual, y(t_actual))
+plot(t_aprox, y_aprox, 'r');
+xlabel('t')
+ylabel('y')
+legend(func_name, 'Approximate')
+title('Euler’s Method')
